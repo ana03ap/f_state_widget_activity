4a521f1bd9e57c1752b9175e67491ebfa02bbc97
Ana María Ardila Pacheco <90521002+ana03ap@users.noreply.github.com>
2025-02-19 10:29:59 -0500

    Update w2.dart

--- lib/ui/widget/w2.dart
+++ lib/ui/widget/w2.dart
@@ -1,15 +1,28 @@
 import 'package:flutter/material.dart';
 
 class W2 extends StatelessWidget {
-  const W2({super.key});
-  final double value = 0;
+  const W2({super.key, required this.value});
+  final double value;
+  
   @override
   Widget build(BuildContext context) {
+
+
+
+    double valuePrueba ;
+    
+    valuePrueba= double.parse(value.toStringAsFixed(1))- value.toInt();
+    valuePrueba = valuePrueba<0?valuePrueba*-1: valuePrueba;
+    print(valuePrueba);
+
     return Container(
       key: const Key('W2'),
       width: 200,
       height: 200,
-      color: Colors.blueGrey, // should be grey when the decimal is bellow 0.4
+      color: valuePrueba<=0.4?  Colors.blueGrey:   const Color.fromARGB(255, 12, 147, 214), // should be grey when the decimal is bellow 0.4
     );
   }
+
+
 }
+
